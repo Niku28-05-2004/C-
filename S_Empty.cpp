e0d89605352d770249340a732ff9cbc98175a51f
Snehal Shete <148787356+Niku28-05-2004@users.noreply.github.com>
2023-12-05 23:56:49 +0530

    Add files via upload

---
+++ S_Empty.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+    string str=" Snehal ";
+    if(str.empty())
+     cout<<"string is empty"<<endl;
+     else
+     cout<<"String is not empty"<<endl;
+     return 0;
+}
