b4e665808782556d2c82fa8290d02ffba18615a5
Snehal Shete <148787356+Niku28-05-2004@users.noreply.github.com>
2023-12-06 00:03:54 +0530

    Add files via upload

---
+++ Compare_str.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+    string str1="HELLO";
+    string str2="hello";
+    cout<<str1.compare(str2);
+    cout<<endl;
+    return 0;
+}
