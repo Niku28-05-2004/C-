54d5a26c7582ba4234186b4eba0497d338a046ee
Snehal Shete <148787356+Niku28-05-2004@users.noreply.github.com>
2023-12-05 23:53:30 +0530

    Add files via upload

---
+++ Strcat.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+    char s1[20]="Good";
+    char s2[10]="Moring";
+    strcat(s1,s2);
+    cout<<s1<<endl;
+    return 0;
+}
