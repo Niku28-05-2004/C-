a8a6e50de782238664872741ed77811d76f6bcc2
Snehal Shete <148787356+Niku28-05-2004@users.noreply.github.com>
2023-12-05 23:43:16 +0530

    Add files via upload

---
+++ Max_number_funct.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int max(int a,int b,int c)
+{
+if(a>b && a>c)
+ return a;
+else if(b>c)
+ return b;
+else  
+  return c;
+}
+int main()
+{
+    int x=10,y=15,z=5,r;
+    r=max(x,y,z);
+cout<<r<<endl;
+return 0;
+}
