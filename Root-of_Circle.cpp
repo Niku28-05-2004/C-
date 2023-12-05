1b94b8e5a53616d493cdcfb78f17346125e2e57b
Snehal Shete <148787356+Niku28-05-2004@users.noreply.github.com>
2023-12-05 23:25:51 +0530

    Add files via upload

---
+++ Root-of_Circle.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float area;
+    float r;
+    cout<<"Enter the radius :";
+    cin>>r;
+    area=3.14*r*r;
+    cout<<"Area of circle is :"<<area;
+    return 0;
+}
