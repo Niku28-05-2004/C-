6735fbf74ba2f022e34f0ec551bdbf8b5613217f
Snehal Shete <148787356+Niku28-05-2004@users.noreply.github.com>
2023-12-06 00:01:27 +0530

    Add files via upload

---
+++ Swap_str.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+    string str;
+    string str1="Programming";
+    string str2="Language";
+    str1.swap(str2);
+    cout<<str1<<endl;
+    cout<<str2<<endl;
+    return 0;
+}
