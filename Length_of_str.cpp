604a88891e75fb255d12120c56c28e2221bae884
Snehal Shete <148787356+Niku28-05-2004@users.noreply.github.com>
2023-12-06 00:04:48 +0530

    Add files via upload

---
+++ Length_of_str.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+    string str="Welcome";
+    int count=0;
+    for(int i=0;str[i]='\0';i++)
+    {
+        count++;
+    }
+    cout<<"Length is "<<count<<endl;
+    return 0;
+}
